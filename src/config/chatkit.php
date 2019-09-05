8a93a5a0c8de75ff1fb98fde735c9d6a44febe26
rameez rami <rameespu@gmail.com>
2019-09-05 14:16:54 +0530

    config sample removed

--- src/config/chatkit.php
+++ src/config/chatkit.php
@@ -11,8 +11,8 @@ return [
     |
     */
     'config' =>[
-        'instance_locator' => env('CHATKIT_INSTANCE_LOCATOR', 'v1:us1:242ae889-2e35-45ab-a3fd-325cb1e8a563'),
-        'key' => env('CHATKIT_SECRET_KEY', 'd6ee9ff2-e88d-4342-8e93-6ea286cac968:df8LHZdVZo3lAnBnw4csnKfTtZ0VQqMPj56L5xOY2EY='),
+        'instance_locator' => env('CHATKIT_INSTANCE_LOCATOR', ''),
+        'key' => env('CHATKIT_SECRET_KEY', ''),
         'port' => env('CHATKIT_PORT',80),
         'timeout' => env('CHATKIT_TIMEOUT',30),
         'debug' => env('CHATKIT_DEBUG',false),
